161548e0c49ceb7b04f1dcba4686a03fbee0c3f8
taniabrancabaleca-hue <taniabrancabaleca@gmail.com>
2026-04-20 23:09:53 +0100

    Change Supabase client import to server version

--- app/pedidos/novo/page.tsx
+++ app/pedidos/novo/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/lib/supabase'
+import { createClient } from '@/lib/supabase-server'
 import NovoPedidoForm from '../NovoPedidoForm'
 
 export const dynamic = 'force-dynamic'
