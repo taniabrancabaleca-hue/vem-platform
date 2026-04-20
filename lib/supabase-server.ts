adb9c4645d1f6abffa47c0a5a6fab0169df3b22e
taniabrancabaleca-hue <taniabrancabaleca@gmail.com>
2026-04-20 15:55:32 +0100

    Add type annotations to setAll method

--- lib/supabase-server.ts
+++ lib/supabase-server.ts
@@ -1,3 +1,4 @@
+
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
@@ -11,7 +12,7 @@ export function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
