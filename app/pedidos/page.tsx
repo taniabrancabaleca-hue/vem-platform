c283649cb94cd855adadd999da67b852b0c2f455
taniabrancabaleca-hue <taniabrancabaleca@gmail.com>
2026-04-20 23:56:32 +0100

    Update import from supabase to supabase-server

--- app/pedidos/page.tsx
+++ app/pedidos/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from '@/lib/supabase'
+import { createClient } from '@/lib/supabase-server'
 import PedidosClient from './PedidosClient'
 
 export const dynamic = 'force-dynamic'
